81136c5122230cedc0e42e67320db0a946f34b13
TraineeNash <syafiq.nasrul106@gmail.com>
2026-04-30 16:21:34 +0800

    The logic is done, understood but not fully. Will learn again and go through. Now onto the design!!

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,17 +1,41 @@
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 
 const Stopwatch = () => {
-    const [time, setTime] = useState(0)
-    const [isrunning, setisrunning] = useState(false)
+    const [time, setTime] = useState(0)                 // counter
+    const [isrunning, setisrunning] = useState(false)   // flag
+    
+    // reference for timer ID
+    // will use with setInterval()
+    const timerRef = useRef(null)                       
+
+
+    const start = () => {
+        if (!isrunning) {
+            setisrunning(true)
+            timerRef.current = setInterval(() => {
+                setTime((second) => second + 1)
+            }, 1000)
+        }
+    }
+
+    const stop = () => {
+        clearInterval(timerRef.current)
+        setisrunning(false)
+    }
+
+    const reset = () => {
+        clearInterval(timerRef.current)
+        setisrunning(false)
+        setTime(0)
+    }
 
     return (
         <div>
-            <h1>Time: {time}</h1>
+            <h1>Time: {time} seconds</h1>
             <p>Running: {isrunning ? "Yes" : "No"}</p> {/* show flag */}
-            <button onClick={() => setTime(time + 1)}>+1</button>
-            <button onClick={() => setTime(0)}>Reset</button>
-            <button onClick={() => setisrunning(true)}>Set Running</button>
-            <button onClick={() => setisrunning(false)}>Set Not Running</button>
+            <button onClick={start}>Start</button>
+            <button onClick={stop}>Stop</button>
+            <button onClick={reset}>Reset</button>
         </div>
     )
 }
