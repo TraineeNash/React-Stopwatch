3696bd124c9268702a4f129953e991efc1758061
TraineeNash <syafiq.nasrul106@gmail.com>
2026-04-30 16:08:02 +0800

    Step 2 done to add flag, a variable to control state whether true or false

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -2,12 +2,16 @@ import React, { useState } from "react"
 
 const Stopwatch = () => {
     const [time, setTime] = useState(0)
+    const [isrunning, setisrunning] = useState(false)
 
     return (
         <div>
             <h1>Time: {time}</h1>
+            <p>Running: {isrunning ? "Yes" : "No"}</p> {/* show flag */}
             <button onClick={() => setTime(time + 1)}>+1</button>
             <button onClick={() => setTime(0)}>Reset</button>
+            <button onClick={() => setisrunning(true)}>Set Running</button>
+            <button onClick={() => setisrunning(false)}>Set Not Running</button>
         </div>
     )
 }
