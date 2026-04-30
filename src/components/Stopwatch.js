18f869bda28c32fef168b6b051f03c8178b823cc
TraineeNash <syafiq.nasrul106@gmail.com>
2026-04-30 15:58:48 +0800

    Latest update to my React stopwatch

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,7 +1,15 @@
-import React from "react"
+import React, { useState } from "react"
 
 const Stopwatch = () => {
-    return <h1>Stopwatch Component</h1>
+    const [time, setTime] = useState(0)
+
+    return (
+        <div>
+            <h1>Time: {time}</h1>
+            <button onClick={() => setTime(time + 1)}>+1</button>
+            <button onClick={() => setTime(0)}>Reset</button>
+        </div>
+    )
 }
 
 export default Stopwatch
