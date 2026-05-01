8ee8e50e34723f906e4b97dedc45129d5f64652a
TraineeNash <syafiq.nasrul106@gmail.com>
2026-05-01 14:06:18 +0800

    Finalize the react app and design! I did it!! Also add improvement text file as suggestion and to test my understanding about react. Let us go!!!

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -29,13 +29,33 @@ const Stopwatch = () => {
         setTime(0)
     }
 
+    const formatTime = (seconds) => {
+        const hrs = String(Math.floor(seconds / 3600)).padStart(2,"0")
+        const mins = String(Math.floor((seconds % 3600) / 60)).padStart(2,"0")
+        const secs = String(seconds % 60).padStart(2,"0")
+        // const secs = String(seconds).padStart(2,"0")
+        return `${hrs} : ${mins} : ${secs}`
+    }
+
     return (
-        <div>
-            <h1>Time: {time} seconds</h1>
-            <p>Running: {isrunning ? "Yes" : "No"}</p> {/* show flag */}
-            <button onClick={start}>Start</button>
-            <button onClick={stop}>Stop</button>
-            <button onClick={reset}>Reset</button>
+        <div className="bg-pink-300 flex flex-col h-screen items-center justify-center gap-2">
+            <div className="flex gap-5">
+                <h1 className="bg-white text-[100px] p-2 rounded-md md:text-[150px]"
+                >{formatTime(time)}</h1>
+                {/* <p>Running: {isrunning ? "Yes" : "No"}</p> show flag */}
+            </div>
+
+            <div className="flex gap-5">
+                <button className="bg-green-800 p-2 rounded-xl text-2xl font-bold text-white" 
+                onClick={start}>
+                    Start</button>
+                <button className="bg-red-800 p-2 rounded-xl text-2xl font-bold text-white" 
+                onClick={stop}>
+                    Stop</button>
+                <button className="bg-gray-600 p-2 rounded-xl text-2xl font-bold text-white" 
+                onClick={reset}>
+                    Reset</button>
+            </div>
         </div>
     )
 }
